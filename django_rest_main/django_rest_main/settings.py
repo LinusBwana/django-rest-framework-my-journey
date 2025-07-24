096cbc252e0cad04d8c9b4838e58f001cfda0353
LinusBwana <linusbwana49@gmail.com>
2025-07-25 00:10:29 +0300

    Created superuser and the student model and made migrations
    -Created a superuser
    -Used postgresql as my database
    -Registered my model on student admin.py

--- django_rest_main/django_rest_main/settings.py
+++ django_rest_main/django_rest_main/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'django_rest_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_rest_main',
+        'USER': 'postgres',
+        'PASSWORD': 'Blue@1996',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
--- django_rest_main/students/admin.py
+++ django_rest_main/students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Student
 
 # Register your models here.
+class studentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'branch')
+
+admin.site.register(Student, studentAdmin)
