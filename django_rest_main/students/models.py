bccd71ac29d27425ab568ecae737302d429d7315
LinusBwana <linusbwana49@gmail.com>
2025-07-25 16:41:20 +0300

    Created Employees app with Employee model and admin configuration
    - Created a new Django app named 'Employees'
    - Defined the Employee model with required fields
    - Registered the model in admin.py
    - Configured admin to display all fields using list_display
    - Ran migrations to create the Employee table in the database

--- django_rest_main/students/models.py
+++ django_rest_main/students/models.py
@@ -6,5 +6,8 @@ class Student(models.Model):
     name = models.CharField(max_length=50)
     branch = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ['id']
