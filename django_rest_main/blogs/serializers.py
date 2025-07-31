89228948bde4dd059b8d382a8f8de792b4daed4a
LinusBwana <linusbwana49@gmail.com>
2025-07-31 14:25:31 +0300

    Added serializers for Blog and Comment models
    - Implemented BlogSerializers and CommentSerializers using ModelSerializer
    - Included all model fields for both serializers

---
+++ django_rest_main/blogs/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Blog, Comment
+
+class BlogSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+class CommentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
