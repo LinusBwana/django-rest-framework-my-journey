a80c0561f34ed0efdabc82d0038c0c6a47989aab
LinusBwana <linusbwana49@gmail.com>
2025-07-31 15:03:56 +0300

    Added nested comments to Blog serializer (read-only)
    - Updated BlogSerializers to include nested CommentSerializers
    - Enabled read-only nested serialization of related comments in blog response

--- django_rest_main/blogs/serializers.py
+++ django_rest_main/blogs/serializers.py
@@ -1,12 +1,15 @@
+from typing import ReadOnly
 from rest_framework import serializers
 from .models import Blog, Comment
-
-class BlogSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Blog
-        fields = '__all__'
 
 class CommentSerializers(serializers.ModelSerializer):
     class Meta:
         model = Comment
+        fields = '__all__'
+
+
+class BlogSerializers(serializers.ModelSerializer):
+    comments = CommentSerializers(many=True, read_only=True)
+    class Meta:
+        model = Blog
         fields = '__all__'
